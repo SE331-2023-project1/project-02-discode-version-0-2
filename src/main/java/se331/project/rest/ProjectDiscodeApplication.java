65703515fe13e04afdb3160c0a16b832045ff15a
deityzs <kongkonghihi@gmail.com>
2023-10-17 01:25:03 +0700

    Init application

---
+++ src/main/java/se331/project/rest/ProjectDiscodeApplication.java
@@ -0,0 +1,13 @@
+package se331.project.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDiscodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectDiscodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se331/project/rest/ProjectDiscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package se331.project.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDiscodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
